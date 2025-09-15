53bcfe08d9e420ff7cd95e883d482c0c82966abe
blackphoenix369 <30rohit2004@gmail.com>
2025-09-15 20:27:25 +0530

    change the function

--- backend/database/db.js
+++ backend/database/db.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = await mysql.createConnection({
-  host: process.env.DB_HOST || "localhost",
+  host: process.env.DB_HOST || "localhost" || "54.191.253.12" || "44.226.122.3",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASS || "Rohit@2025",
   database: process.env.DB_NAME || "artisan_market",
