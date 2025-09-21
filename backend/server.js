eea5fe41a43d7ea4753665c4a584ed458867b916
blackphoenix369 <30rohit2004@gmail.com>
2025-09-21 20:07:51 +0530

    changes made to backend/server.js to solve database problem

--- backend/server.js
+++ backend/server.js
@@ -25,5 +25,5 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend", "index.html"));
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
